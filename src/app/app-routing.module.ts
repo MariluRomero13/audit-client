4da7ec6cfc053a9242d211abe85c302158bc7877
MariluRomero13 <romeromarilu13@gmail.com>
2020-10-10 11:37:30 -0500

    [UPD] routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { PageNotFoundComponent } from './components/structure/page-not-found/pag
 import { dashboardRoutes } from './components/structure/dashboard.routes';
 
 const appRoutes: Routes = [
-  { path: 'panel', redirectTo: '/panel/dashboard', pathMatch: 'full' },
-  { path: '', redirectTo: '/panel/dashboard', pathMatch: 'full' },
+  // { path: 'panel', redirectTo: '/panel/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/panel', pathMatch: 'full' },
   { path: 'panel', component: AdminLayoutComponent, children: dashboardRoutes},
   { path: '**', component: PageNotFoundComponent }
 ];
